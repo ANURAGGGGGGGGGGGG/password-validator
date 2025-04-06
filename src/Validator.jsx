65702fec0c76ca0a781a97a2aba88e4d41828f89
Anurag <144671360+ANURAGGGGGGGGGGGG@users.noreply.github.com>
2025-04-06 11:37:47 +0530

    Initial commit for Password-Generator

--- src/Validator.jsx
+++ src/Validator.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import validator from 'validator';
 import './Validator.css';
@@ -103,34 +102,3 @@ const Validator = () => {
 };
 
 export default Validator;
-=======
-import React, { useState } from 'react'
-import validator from 'validator'
-const Validator = () => {
-    const [errorMessage, setErrorMessage] = useState(' ')
-    const validate = (value) => {
-        if (validator.isStrongPassword(value, {
-            minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
-        })) {
-            setErrorMessage('Your Password is Strong')
-        } else {
-            setErrorMessage('Is not strong Password')
-        }
-    }
-    return (
-        <>
-            <div>
-                <h2>Checking Password Strength in React JS</h2>
-                <label htmlFor=""> Enter Password</label>
-                <input type="text" onChange={(e) => validate(e.target.value)} />
-                {
-                    errorMessage === "" ? null :
-                        <div style={{ color: 'red' }}>{errorMessage}</div>
-                }
-            </div>
-        </>
-    )
-}
-
-export default Validator
->>>>>>> d1e6162d0df1d99a1e174c02249d5b344eedf44d
